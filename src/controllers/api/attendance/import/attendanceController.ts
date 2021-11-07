1546d2ca213d36daa68ef87b684910899e5836d4
Wisnu <wisnuaryadipa93@gmail.com>
2021-11-07 16:02:15 +0700

    parse excel to json

--- src/controllers/api/attendance/import/attendanceController.ts
+++ src/controllers/api/attendance/import/attendanceController.ts
@@ -6,33 +6,35 @@ import xlsx from 'xlsx';
 class Controller extends BaseController {
 
     requestHandler = async (req: Request, res: Response) => {
-        this.sendResponse(req, res, {data: req.file});
+        // this.sendResponse(req, res, {data: req.file});
         let option: IOptions = {};
         try {
-            if (req.body == undefined) {
+            if (req.file == undefined) {
                 option.status = 400;
                 option.message = "Please upload an excel file!";
                 this.sendResponse(req, res, option);
+            } else {
+                let attendanceJson = this.parseExcelToJson(req.file);
+                option.status = 200;
+                option.message = "success";
+                option.data = attendanceJson;
+                this.sendResponse(req, res, option);
             }
-            let importedExcel = xlsx.read(req.body, {type: 'buffer'})
-            let firstSheetName = importedExcel.SheetNames[0];
-
-            let worksheet = importedExcel.Sheets[firstSheetName];
-            
-            option.status = 200;
-            option.message = "success";
-            option.data = importedExcel;
-            this.sendResponse(req, res, option);
-            
         }
         catch (err) {
             console.log(err);
-            
             option.status = 500;
             option.message = "Could not upload the file";
             this.sendResponse(req, res, option);
         }
         
+    }
+
+    parseExcelToJson = (excelFile: Express.Multer.File) => {
+        let wb = xlsx.read(excelFile?.buffer, {type: 'buffer'});
+        let worksheet = wb.Sheets[wb.SheetNames[0]];
+        let jsonXlsx = xlsx.utils.sheet_to_json(worksheet);
+        return JSON.stringify(jsonXlsx);
     }
 }
 

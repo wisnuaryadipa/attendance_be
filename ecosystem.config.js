0b9f84ee92ffb5bcac202d4cfc1b7ce2ab9de408
Wisnu <wisnuaryadipa93@gmail.com>
2021-12-30 12:02:11 +0700

    add ecosystem config for pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apps : [{
+      name   : "api-attendance",
+      script : "./src/app.ts",
+      interoreter: 'node',
+      interpreter_args: '-- -r tsconfig-paths/register -r ts-node/register',
+      merge_logs: true,
+      max_restarts: 20,
+    }]
+  }
--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,7 +1,7 @@
 import expressLoader from '@src/loaders/express';
 import express from 'express';
 import sequelizeLoader from '@src/loaders/sequelize';
-import Logger from '@src/loaders/logger';
+import Logger from 'src/loaders/logger';
 
 
 const loaders = async ({ expressApp } : {expressApp: express.Application}) => {

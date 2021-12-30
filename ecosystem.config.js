ac665c0e3e40d5b13eebfb6da1b8c4d6fa18c793
Wisnu <wisnuaryadipa93@gmail.com>
2021-12-30 14:34:35 +0700

    Update for pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,7 @@ module.exports = {
     apps : [{
       name   : "api-attendance",
       script : "./src/app.ts",
-      interoreter: 'node',
-      interpreter_args: '-- -r tsconfig-paths/register -r ts-node/register',
+      interpreter_args: '-r tsconfig-paths/register -r ts-node/register',
       merge_logs: true,
       max_restarts: 20,
     }]
--- src/loaders/sequelize.ts
+++ src/loaders/sequelize.ts
@@ -1,4 +1,4 @@
-import {Sequelize} from 'Sequelize';
+import {Sequelize} from 'sequelize';
 import config from '@src/config/sequelize'
 
 const db = new Sequelize(config.database, config.username, config.password, config); 

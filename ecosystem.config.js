afeaac1238747d9af5a849fd23860926a1ebc056
Wisnu <wisnuaryadipa93@gmail.com>
2021-12-30 14:49:06 +0700

    Update for pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,6 +2,7 @@ module.exports = {
     apps : [{
       name   : "api-attendance",
       script : "./src/app.ts",
+      interpreter: 'node',
       interpreter_args: '-r tsconfig-paths/register -r ts-node/register',
       merge_logs: true,
       max_restarts: 20,

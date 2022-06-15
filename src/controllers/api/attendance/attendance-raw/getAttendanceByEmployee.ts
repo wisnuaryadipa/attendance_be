ac78e21817b1d640509a89bfd95c0776b367516e
Macbook Air <wisnuaryadipa93@gmail.com>
2022-06-15 23:46:18 +0700

    Add Joi on attendance RAW

--- src/controllers/api/attendance/attendance-raw/getAttendanceByEmployee.ts
+++ src/controllers/api/attendance/attendance-raw/getAttendanceByEmployee.ts
@@ -1,5 +1,6 @@
 import { BaseController } from "../../baseController";
 import { Request, Response } from "express";
+import Joi from 'joi';
 
 interface IRequests {
 
@@ -12,9 +13,30 @@ interface IRequests {
 
 class Controller extends BaseController {
 
+    requestValidationSchema = {
+        body: Joi.object({}).required(),
+        query: Joi.object({
+            limit: Joi.number(),
+            dateStart: Joi.string(),
+            dateEnd: Joi.string(),
+        }).required(),
+        params: Joi.object({
+            employeeId: Joi.number().required()
+        }).required(), 
+        header: Joi.object({}).required()
+    }
+
     requestHandler = async (req: Request, res: Response) => {
 
+        let _reqVal = await this.validateRequest(req);
+        let {params, query} = _reqVal;
 
+        let _req: IRequests = {
+            dateEnd: query.dateEnd,
+            dateStart: query.dateStart,
+            employeeId: params.employeeId,
+            limit: (query.limit) ? parseInt(query.limit.toString()) : 10
+        }
 
         let _result;
 
